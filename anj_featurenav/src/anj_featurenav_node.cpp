60ec6ead7834599b139a5e13e07a67d552ce5758
Gaël Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2015-02-20 09:22:12 +0100

    Remove forced debug mode

--- anj_featurenav/src/anj_featurenav_node.cpp
+++ anj_featurenav/src/anj_featurenav_node.cpp
@@ -8,7 +8,6 @@
 #include <string>
 
 #include <ros/ros.h>
-#include <ros/console.h> // to change the log level to debug
 
 #include <anj_featurenav/jockey.h>
 
@@ -18,12 +17,6 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "anj_featurenav");
   ros::NodeHandle private_nh("~");
   
-  // Debug log level
-  if(ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug))
-  {
-    ros::console::notifyLoggerLevelsChanged();
-  }
-
   std::string jockey_base_name;
   std::string default_jockey_base_name = ros::this_node::getName();
   private_nh.param<std::string>("jockey_base_name", jockey_base_name, default_jockey_base_name);
